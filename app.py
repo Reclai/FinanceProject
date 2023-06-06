bd9a6839976be5762e596ff6df7a6228bc7fa05a
unknown <waynetheinerafter@gmail.com>
2023-06-06 15:27:24 +0700

    Login Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+client = MongoClient("mongodb+srv://test:sparta@pymongolx.npweo5y.mongodb.net/?retryWrites=true&w=majority")
+db = client["FinanceProject"]
+SECRET_KEY = "SECRET_KEY"
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    token_receive = request.cookies.get("mytoken")
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=["HS256"])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template("index.html", user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="Your token has expired"))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="There was problem logging you in"))
+
+@app.route("/login")
+def login():
+    msg = request.args.get("msg")
+    return render_template("login.html", msg=msg)
