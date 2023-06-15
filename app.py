5df05f592b22a128ff1b2828f4ec580248152da7
Timothy Erlangga <timodevops@gmail.com>
2023-06-15 20:15:29 +0700

    History Page

--- app.py
+++ app.py
@@ -191,6 +191,19 @@ def contact():
         return redirect(url_for("login", msg="Your token has expired"))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="There was problem logging you in"))
+    
+@app.route("/history")
+def history():
+    token_receive = request.cookies.get("mytoken")
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=["HS256"])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template("history.html", user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="Your token has expired"))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="There was problem logging you in"))
+    
 
 @app.route("/addContact", methods=['POST'])
 def addContact():
